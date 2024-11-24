681a8e18f012dbeb3b6117e7c0e7caefb02760ff
Satrio <satrioutomo344@gmail.com>
2024-11-25 05:43:06 +0700

    first commit

---
+++ Print week2-1.py
@@ -0,0 +1,7 @@
+print ("\033[1mUNIVERSITAS TIDAR\033[0m")
+print ("Universitas Tidar merupakan Perguruan Tinggi Pemerintah atau Perguruan Tinggi Negeri yang telah ditetapkan berdasarkan Peraturan Presiden Republik Indonesia Nomor 23 Tahun 2014 pada tanggal 1 April 2014")
+print ("\n\033[1mJURUSAN TEKNIK ELEKTRO, MEKATRONIKA, DAN INFORMASI\033[0m")
+print ("Jurusan Teknik Elektro terdiri atas tiga program studi, antara lain:")
+print ("  1. S1 Teknik Elektro (\x1b[3mBachelor of Electrical Engineering)")
+print ("  2. S1 Teknologi Informasi (\x1b[3mBachelor of Information Technology)")
+print ("  3. S1 Teknik Mekatronika (\x1b[3mBachelor of Mechatronics Engineering)")
---
+++ Print week2.py
@@ -0,0 +1,5 @@
+print ("Nama: Satrio Putro Utomo")
+print ("\nNPM: 2--05---63")
+print ("\nAlamat: Jln. Bougenville No. RT--- RW--, Kab. Magelang")
+print ("\nTinggi Badan: 170")
+print ("\nBerat Badan: 65")
